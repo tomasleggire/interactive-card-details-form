5b74170b6f1a4e2979c3336d16550714762ae90b
Tomás Leggire <tomasleggire@hotmail.com>
2023-01-24 20:20:48 -0300

    Cards terminadas

--- src/COMPONENTS/Cards.js
+++ src/COMPONENTS/Cards.js
@@ -5,10 +5,18 @@ export default function Cards({images}) {
     return (
         <div className="cards-main">
           <div className="card card-front" style={{ backgroundImage: `url(${images.bgCardFront})` }}>
-
+            <div className="card-logo-container">
+              <img src={images.cardLogo}/>
+            </div>
+            <p className="p-front">0000 0000 0000 0000</p>
+            <div className="info-front">
+                <h6 className="name-front">JANE APPLESEED</h6>
+                <span className="span-front">00/00</span>
+            </div>
           </div>
+
           <div className="card card-back" style={{ backgroundImage: `url(${images.bgCardBack})` }}>
-            
+            <span className="span-back">000</span>
           </div>
         </div>
     )
--- src/HOOKS/useCardForm.js
+++ src/HOOKS/useCardForm.js
@@ -4,6 +4,8 @@ import bgMobile from '../IMAGES/bg-main-mobile.png';
 import bgCardFront from '../IMAGES/bg-card-front.png';
 import bgCardBack from '../IMAGES/bg-card-back.png';
 
+import cardLogo from '../IMAGES/card-logo.svg';
+
 export default function useCardForm() {
 
     const images = {
@@ -11,6 +13,7 @@ export default function useCardForm() {
         bgMobile: bgMobile,
         bgCardFront: bgCardFront,
         bgCardBack: bgCardBack,
+        cardLogo: cardLogo,
     }
 
     return [images];
