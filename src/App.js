7376b01d5dbe5d33678730a774d3d49499f3531a
Tomás Leggire <tomasleggire@hotmail.com>
2023-01-24 18:45:20 -0300

    bg-desktop puesto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import '../src/CSS/App.css';
+import bgDesktop from '../src/IMAGES/bg-main-desktop.png';
 
 function App() {
   return (
     <div className='app-main'>
+      <div className='img-back-desktop'>
+        <img src={bgDesktop} />
+      </div>
     </div>
   );
 }
