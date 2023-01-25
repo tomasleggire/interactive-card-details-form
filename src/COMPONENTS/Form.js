51ebf06a3e81959eabba80a3a98e9f8887cb3df8
Tomás Leggire <tomasleggire@hotmail.com>
2023-01-25 19:01:07 -0300

    Avances en exp.date

--- src/COMPONENTS/Form.js
+++ src/COMPONENTS/Form.js
@@ -7,7 +7,16 @@ export default function Form({setState}) {
             <label className="name-label">CARDHOLDER NAME</label>
             <input type='text' placeholder="e.g. Jane Appleseed" className="name-input"/>
             <label className="number-label">CARD NUMBER</label>
-            <input type='text' placeholder="e.g. 1234 5678 9123 0000" className="number-input"/>
+            <input type='text' placeholder="e.g. 1234 5678 9123 0000" className="number-input" maxLength='16'/>
+            <div className="last-info-main">
+                <div className="exp-date">
+                    <label className="exp-label">{`EXP. DATE (MM/YY)`}</label>
+                    <div className="date-inputs">
+                        <input type='text' className="input-date-mm" placeholder="MM" pattern="\d*" maxlength="2"/>
+                        <input type='text' className="input-date-yy" placeholder="YY" pattern="\d*" maxlength="2"/>
+                    </div>
+                </div>
+            </div>
         </form>
     )
 }
