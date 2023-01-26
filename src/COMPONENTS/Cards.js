3a7ca7f1c78427b5d29e07e913419691c92f2038
Tomás Leggire <tomasleggire@hotmail.com>
2023-01-26 18:56:15 -0300

    Arreglos en cards

--- src/COMPONENTS/Cards.js
+++ src/COMPONENTS/Cards.js
@@ -8,7 +8,24 @@ export default function Cards({images, state}) {
             <div className="card-logo-container">
               <img src={images.cardLogo} alt='Card Logo'/>
             </div>
-            <p className="p-front">{state.cardNumber}</p>
+            <p className="p-front">{
+              state.cardNumber[0] +
+              state.cardNumber[1] +
+              state.cardNumber[2] +
+              state.cardNumber[3] + ' ' +
+              state.cardNumber[4] +
+              state.cardNumber[5] +
+              state.cardNumber[6] +
+              state.cardNumber[7] + ' ' +
+              state.cardNumber[8] +
+              state.cardNumber[9] +
+              state.cardNumber[10] +
+              state.cardNumber[11] + ' ' +
+              state.cardNumber[12] +
+              state.cardNumber[13] +
+              state.cardNumber[14] +
+              state.cardNumber[15] 
+            }</p>
             <div className="info-front">
                 <h6 className="name-front">{state.cardName}</h6>
                 <span className="span-front">{`${state.expiration[0]}/${state.expiration[1]}`}</span>
